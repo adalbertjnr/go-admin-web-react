6f34c8875fa4a0939e6d61043641c08f881b08f8
Adalberto Junior <adalberto.junior@baseservice.io>
2023-12-16 23:22:51 -0300

    refactoring logout handler

--- handlers/logout.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import "github.com/gofiber/fiber/v2"
-
-func (g StoreDB) Logout(c *fiber.Ctx) error {
-	c.ClearCookie("hunter-123-token")
-	c.Status(fiber.StatusOK)
-	return c.JSON(fiber.Map{"message": "successful logout"})
-}
