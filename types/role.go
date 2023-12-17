f5809e7ccd4df7af8a497292401b2816f0cd995e
Adalberto Junior <adalberto.junior@baseservice.io>
2023-12-16 23:25:40 -0300

    role type many to many

--- types/role.go
+++ types/role.go
@@ -1,6 +1,7 @@
 package types
 
 type Role struct {
-	Id   uint   `json:"id"`
-	Name string `json:"name"`
+	Id          uint         `json:"id"`
+	Name        string       `json:"name"`
+	Permissions []Permission `json:"permission" gorm:"many2many:role_permissions"`
 }
